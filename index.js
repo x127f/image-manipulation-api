c25b5e8db5e9fbda050d728c6defb5440e92bb3a
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2020-08-23 11:18:10 +0200

    skip hash for development

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ require("express-async-errors");
 const app = express();
 const port = 3000;
 const routes = require("./routes");
+const config = require("./config.json");
 
+app.use((req, res, next) => {
+	if (!config.production) return next();
+	if (!req.query || !Object.keys(req.query).length || !req.query.hash) throw "No query/hash specified";
+	var payload = { ...req.query };
+	delete payload.hash;
+	payload = JSON.stringify(payload);
 
-// app.use((req, res, next) => {
-// 	if (!req.query || !Object.keys(req.query).length || !req.query.hash) throw "No query/hash specified";
-// 	var payload = { ...req.query };
-// 	delete payload.hash;
-// 	payload = JSON.stringify(payload);
+	var hash = crypto.createHmac("sha256", secret).update(payload).digest("hex");
 
-// 	var hash = crypto.createHmac("sha256", secret).update(payload).digest("hex");
+	if (hash !== req.query.hash) throw "Invalid query signature";
 
-// 	if (hash !== req.query.hash) throw "Invalid query signature";
-
-// 	next();
-// });
+	next();
+});
 
 routes(app);
 
@@ -34,7 +35,9 @@ app.use((err, req, res, next) => {
 			return res.sendFile(__dirname + "/assets/404.jpg");
 		} else {
 			return res.status(400).send({
-				err: err.toString() + ", for further help, view our repo: https://github.com/Trenite/image-manipulation-api",
+				err:
+					err.toString() +
+					", for further help, view our repo: https://github.com/Trenite/image-manipulation-api",
 				success: false,
 				status: 400,
 			});
