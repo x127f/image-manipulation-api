b5d1d34325d7f498b5d67d7c8258a9c915772598
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2020-08-25 01:47:52 +0200

    :zap: cluster process

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-const express = require("express");
-const crypto = require("crypto");
-const fs = require("fs");
-// const { registerFont } = require("canvas");
-
-require("express-async-errors");
 const config = require("./config.json");
-const app = express();
-const port = config.port;
-const routes = require("./routes");
-const errorHandler = require("./routes/error");
+var cluster = require("cluster");
+var path = require("path");
 
-app.use((req, res, next) => {
-	if (!config.production) return next();
-	if (!req.query || !Object.keys(req.query).length || !req.query.hash) throw "No query/hash specified";
-	var payload = { ...req.query };
-	delete payload.hash;
-	payload = JSON.stringify(payload);
+var cores = require("os").cpus().length;
+if (!config.production) cores = 1;
 
-	var hash = crypto.createHmac("sha256", config.secret).update(payload).digest("hex");
-
-	if (hash !== req.query.hash) throw "Invalid query signature";
-
-	next();
-});
-
-routes(app);
-
-app.use(errorHandler);
-
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
+if (cluster.isMaster) {
+	for (var i = 0; i < cores; i++) {
+		cluster.fork();
+	}
+	cluster.on("online", function (worker) {
+		console.log("Worker " + worker.process.pid + " is online.");
+	});
+	cluster.on("exit", function (worker, code, signal) {
+		console.log("worker " + worker.process.pid + " died.");
+		cluster.fork();
+	});
+} else {
+	const server = require("./server");
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const crypto = require("crypto");
+const fs = require("fs");
+// const { registerFont } = require("canvas");
+
+require("express-async-errors");
+const config = require("./config.json");
+const app = express();
+const port = config.port;
+const routes = require("./routes");
+const errorHandler = require("./routes/error");
+
+app.use((req, res, next) => {
+	if (!config.production) return next();
+	if (!req.query || !Object.keys(req.query).length || !req.query.hash) throw "No query/hash specified";
+	var payload = { ...req.query };
+	delete payload.hash;
+	payload = JSON.stringify(payload);
+
+	var hash = crypto.createHmac("sha256", config.secret).update(payload).digest("hex");
+
+	if (hash !== req.query.hash) throw "Invalid query signature";
+
+	next();
+});
+
+routes(app);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
