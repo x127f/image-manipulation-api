331c391c9ab3de0b4da9b49090c3f6ef34bf8807
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2020-08-25 15:50:50 +0200

    :art: [template] enhancment

--- routes/template.js
+++ routes/template.js
@@ -1,5 +1,21 @@
+const { createCanvas, loadImage, Image, registerFont } = require("canvas");
+
 module.exports = (app) => {
-	app.get("/", (req, res) => {
-		return;
+	app.get("/", async (req, res) => {
+		var { user_tag } = req.query;
+		if (!user_tag) {
+			throw "query invalid";
+		}
+
+		const canvas = createCanvas(1000, 300);
+		const { width, height } = canvas;
+		const ctx = canvas.getContext("2d");
+
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, width, height);
+
+		let buffer = canvas.toBuffer("image/jpeg");
+		res.set("Content-Type", "image/jpeg");
+		res.send(buffer);
 	});
 };
