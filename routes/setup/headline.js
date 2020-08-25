fc352c3535a234451f2581927625435859031781
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2020-08-25 23:31:01 +0200

    :bug: [headline] background fix

--- routes/setup/headline.js
+++ routes/setup/headline.js
@@ -1,4 +1,4 @@
-const { createCanvas, loadImage } = require("canvas");
+const Canvas = require("canvas");
 
 module.exports = (app) => {
 	app.get("/", async (req, res) => {
@@ -8,13 +8,19 @@ module.exports = (app) => {
 		if (!font) font = "Arial";
 		if (!shadow) shadow = 0;
 		if (!color) color = "black";
-		if (background) {
-			background = await loadImage(background);
-			// TODO fix background
 
-			if (!size) size = font === "lypix-font" ? background.width / (text.length / 1.3) : background.width / (text.length / 1.5);
-			if (!x) x = background.width / 2;
-			if (!y) y = background.height / 2 - size / 1.5;
+		try {
+			var back = await Canvas.getBackground(background);
+		} catch (error) {}
+
+		if (background && back) {
+			var height = back.height;
+			var width = back.width;
+			if (!size) {
+				size = font === "lypix-font" ? width / (text.length / 1.3) : width / (text.length / 1.5);
+			}
+			if (!x) x = width / 2;
+			if (!y) y = height / 2 - size / 1.5;
 		} else {
 			if (!x) x = 0;
 			if (!y) y = 0;
@@ -30,32 +36,30 @@ module.exports = (app) => {
 		y = parseInt(y);
 		if (isNaN(y)) throw "y is not a number";
 
-		if (background) {
-			height = background.height;
-			width = background.width;
-		} else {
-			height = size + size / 3;
+		if (!back) {
+			var test = Canvas.createCanvas(0, 0).getContext("2d");
+			var height = size + size / 3;
 
-			var test = createCanvas(0, 0).getContext("2d");
 			test.font = `${size}px ${font}`;
-			width = test.measureText(text).width + size;
+			var width = test.measureText(text).width + size;
 		}
 
-		const canvas = createCanvas(width, height);
+		const canvas = Canvas.createCanvas(width, height);
 		const ctx = canvas.getContext("2d");
 
-		ctx.textAlign = background ? "center" : "left";
-		ctx.fillStyle = color;
+		ctx.textAlign = back ? "center" : "left";
 		ctx.font = `${size}px ${font}`;
+		ctx.fillStyle = color;
 
 		ctx.shadowColor = "black";
 		ctx.shadowBlur = shadow;
 		ctx.shadowOffsetX = shadow;
 		ctx.shadowOffsetY = shadow;
 
-		if (background) {
-			ctx.drawImage(background, 0, 0);
+		if (back) {
+			await ctx.drawBackground(back);
 		} else {
+			if (background) await ctx.drawBackground(background);
 			x += size / 2;
 		}
 		ctx.fillText(text, x, y + size);
