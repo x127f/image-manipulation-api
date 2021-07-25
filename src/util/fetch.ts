060bc5b574ebdfdfe4fe6cdf144b0543a0a41c92
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-07-25 21:46:09 +0200

    :zap: caching

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,8 +1,27 @@
 // @ts-nocheck
 import "missing-native-js-functions";
-import f from "node-fetch";
+import path from "path";
+
+function checkFileExists(file: string) {
+	return require("fs")
+		.promises.access(file)
+		.then(() => true)
+		.catch(() => false);
+}
+
+const cache = path.join(__dirname, "..", "..", "cache", "download");
+if (!globalThis.window) {
+	require("fs-extra").ensureDirSync(cache);
+}
 
 export async function fetchBase64(url: string, opts: any) {
+	const file = path.join(cache, url.split("://")[1].replaceAll("/", "-"));
+	if (!globalThis.window) {
+		if (await checkFileExists(file)) {
+			return require("fs").promises.readFile(file, { encoding: "utf8" });
+		}
+	}
+
 	const fetch = globalThis.fetch || require("node-fetch");
 	const FileReader = globalThis.FileReader || require("filereader");
 	const response = await fetch(url, opts);
@@ -18,5 +37,8 @@ export async function fetchBase64(url: string, opts: any) {
 		image = body.toString("base64");
 	}
 
-	return `data:${response.headers.get("content-type")?.toLowerCase()};base64,${image}`;
+	const result = `data:${response.headers.get("content-type")?.toLowerCase()};base64,${image}`;
+	if (!globalThis.window) require("fs").promises.writeFile(file, result, { encoding: "utf8" }).caught(); // do not await file to be written for lower latency
+	if (response.status !== 200) throw new Error("not found");
+	return result;
 }
